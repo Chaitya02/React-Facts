42a3ff84b65d9db9db47094060d4911d5a45a479
Chaitya <chaityadobariya02@gmail.com>
2023-06-22 10:20:09 +0530

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+export default function Navbar() {
+  return (
+    <nav>
+      <div className="brand">
+        <img className="logo" src="./images/react-logo.png" alt="react-logo" />
+        <h2>ReactFacts</h2>
+      </div>
+      <h3>React Course - Project 1</h3>
+    </nav>
+  );
+}
